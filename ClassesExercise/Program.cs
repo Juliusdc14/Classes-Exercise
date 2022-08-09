8a47d2da53416fb6c1accbc972515015855187c1
juliusdc14 <juliusdc05@gmail.com>
2022-08-09 10:21:14 -0500

    Cars exercise

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new car();
+            myCar.Make = "Audi";
+            myCar.Model = "R6";
+            myCar.Year = 2022;
+
+            var mercedes = new car()
+            {
+                Make = "Mercedes",
+                Model = "CLS",
+                Year = 2022
+
+            };
+
+            var carList = new List<car>() { myCar, mercedes };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        }
+    }
+}
